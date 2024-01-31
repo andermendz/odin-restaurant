a261b75da03af5dc01e0f710c9eb242492b8089f
andermendz <andersonmendz19@gmail.com>
2024-01-30 19:42:58 -0500

    basic webpack home section

---
+++ src/home.js
@@ -0,0 +1,20 @@
+import homeimage from './media/home.jpg';
+
+export default function home () {
+    let content = document.querySelector('.content');
+    content.innerHTML = `<div class="home">
+    <div class="texts-container">
+        <div class="title">
+            Welcome to Our Restaurant
+        </div>
+        <div class="sub-text">
+            we're an orchestra of culinary artistry. 
+            Led by Michelin-starred maestros and seasoned veterans,
+             our kitchen hums with a symphony of skills and passion.
+        </div>
+    </div>
+    
+     <img src="${homeimage}" alt="">
+</div>`;
+   
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+import styles from './style.css';
+import home from './home';
+
+
+let content = document.querySelector('.content')
+let homeact = document.querySelector('#home')
+let menuact = document.querySelector('menu')
+let aboutact = document.querySelector('about')
+
+homeact.onclick = home;
